7ef1986d7305d596d6b0213aaf3dc4bcc23a7e50
Kacper <kacper.gajdek2@gmail.com>
2024-07-29 17:52:26 +0200

    Initialized bootcamp v2

--- src/bootcamp_chat_backend/src/lib.rs
+++ src/bootcamp_chat_backend/src/lib.rs
@@ -1,4 +1,17 @@
+use std::{cell::RefCell, collections::HashMap};
+
+thread_local! {
+    static NOTES: RefCell<Vec<String>> = RefCell::default();
+}
+
 #[ic_cdk::query]
-fn greet(name: String) -> String {
-    format!("Hello, {}!", name)
+fn getNotes() -> Vec<String> {
+    NOTES.with_borrow(|notes| notes.clone())
+}
+
+#[ic_cdk::update]
+fn addNote(note: String) {
+    NOTES.with_borrow_mut(|notes|{
+        notes.push(note);
+    })
 }
