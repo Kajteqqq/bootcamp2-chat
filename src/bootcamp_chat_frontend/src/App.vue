2c6b77a784981825e63bca1674d96761dc4e8827
Kacper <kacper.gajdek2@gmail.com>
2024-07-29 19:26:13 +0200

    Koniec dnia pierwszego

--- src/bootcamp_chat_frontend/src/App.vue
+++ src/bootcamp_chat_frontend/src/App.vue
@@ -1,28 +1,43 @@
 <script lang="ts">
 import { ref } from 'vue';
 import { bootcamp_chat_backend } from '../../declarations/bootcamp_chat_backend';
+import { AuthClient } from '@dfinity/auth-client';
+import { HttpAgent } from '@dfinity/agent';
+import type { Identity } from '@dfinity/agent';
 
 export default {
-  data(){
-    return{
+  data() {
+    return {
       newNote: "",
-      notes: [] as string[]
+      notes: [] as string[],
+      identity: undefined as undefined | Identity,
     }
   },
-  methods:{
-    async dodajNotatke(){
-      await bootcamp_chat_backend.addNote(this.newNote)
+  methods: {
+    async dodajNotatke() {
+      await bootcamp_chat_backend.add_note(this.newNote)
       await this.pobierzNotatki()
     },
-    async pobierzNotatki(){
-      this.notes = await bootcamp_chat_backend.getNotes()
+    async pobierzNotatki() {
+      this.notes = await bootcamp_chat_backend.get_notes()
+    },
+    async login() {
+      const authClient = await AuthClient.create();
+      await authClient.login({
+        identityProvider: "http://avqkn-guaaa-aaaaa-qaaea-cai.localhost:4943/"
+      })
+
+      const identity = authClient.getIdentity();
+      console.log("Zalogowano", identity.getPrincipal())
+      this.identity = identity;
+      
+     //const agent = new HttpAgent({ identity });
     }
   },
   mounted(){
     this.pobierzNotatki()
   }
 }
-
 </script>
 
 <template>
@@ -30,6 +45,7 @@ export default {
     <img src="/logo2.svg" alt="DFINITY logo" />
     <br />
     <br />
+    {{ identity?.getPrincipal() }} <button @click="login">Login</button>
     <div>
       {{ notes }}
     </div>
