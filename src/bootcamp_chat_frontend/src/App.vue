a420aeeb4f93b30ad1986021f454778c9effe178
Kacper <kacper.gajdek2@gmail.com>
2024-07-30 16:34:50 +0200

    repaired get_notes error

--- src/bootcamp_chat_frontend/src/App.vue
+++ src/bootcamp_chat_frontend/src/App.vue
@@ -1,9 +1,10 @@
 <script lang="ts">
 import { ref } from 'vue';
-import { bootcamp_chat_backend } from '../../declarations/bootcamp_chat_backend';
+import { bootcamp_chat_backend, canisterId, createActor } from '../../declarations/bootcamp_chat_backend';
 import { AuthClient } from '@dfinity/auth-client';
 import { HttpAgent } from '@dfinity/agent';
 import type { Identity } from '@dfinity/agent';
+import { Principal } from '@dfinity/principal';
 
 export default {
   data() {
@@ -15,24 +16,32 @@ export default {
   },
   methods: {
     async dodajNotatke() {
+      if (!this.identity || this.identity.getPrincipal() === Principal.anonymous()){
+        throw new Error("PLZ log in")
+      }
+      const backend = createActor(canisterId,{
+        agentOptions:{
+          identity: this.identity
+        }
+      });
       await bootcamp_chat_backend.add_note(this.newNote)
       await this.pobierzNotatki()
     },
     async pobierzNotatki() {
-      this.notes = await bootcamp_chat_backend.get_notes()
+      if (!this.identity || this.identity.getPrincipal() === Principal.anonymous()){
+        throw new Error("PLZ log in")
+      }
+      this.notes = await bootcamp_chat_backend.get_notes(this.identity.getPrincipal())
     },
     async login() {
       const authClient = await AuthClient.create();
       await authClient.login({
-        identityProvider: "http://avqkn-guaaa-aaaaa-qaaea-cai.localhost:4943/"
+        identityProvider: "http://asrmz-lmaaa-aaaaa-qaaeq-cai.localhost:4943/"
       })
 
       const identity = authClient.getIdentity();
       console.log("Zalogowano", identity.getPrincipal())
       this.identity = identity;
-      
-      const agent = await HttpAgent.create({ identity: this.identity })
-      this.backend = createActor(canisterId, { agent })
     }
   },
   mounted(){
